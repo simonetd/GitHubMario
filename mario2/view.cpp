d5607e76f6a6e19d6360e0791e223641c156cece
david simonet <david.simonet.b@gmail.com>
2015-11-11 17:22:30 +0100

    background to scene

--- mario2/view.cpp
+++ mario2/view.cpp
@@ -15,11 +15,12 @@ void view::map()
 {
     QGraphicsScene * scene = new QGraphicsScene();
     scene->setSceneRect(0,0,1200,300);
-    setBackgroundBrush(QBrush(QImage(":/images/bg.png")));
+
     QGraphicsView * view = new QGraphicsView(scene);
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setFixedSize(600,300);
+    scene->setBackgroundBrush(QBrush(QImage(":/images/bg.png")));
     for (int i=0; i<control->getmodel()->getFlors()->size();i++){
          scene->addItem(control->getmodel()->getFlors()->at(i));
      }
